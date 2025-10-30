983a1c555a815465675d109c7efaaf18f8be482c
Maria Marrapodi <mchiaramarrapodi@gmail.com>
2025-10-30 13:18:52 +0000

    commit sales margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,8 +6,8 @@ select
     revenue,
     quantity,
     purchase_price,
-    ROUND (s.quantity*p.purchase_price,2) as purchase_cost
-    ROUND (s.revenue-s.quantity*p.purchase_price,2) AS margin
-from {{ref ("stg_gz_raw_data__raw_gz_product")}} s
-LEFT JOIN {{ref ("stg_gz_raw_data__raw_gz_sales")}} p
+    ROUND (quantity*purchase_price,2) as purchase_cost,
+    ROUND (revenue-quantity*purchase_price,2) AS margin
+from {{ref ("stg_gz_raw_data__raw_gz_product")}}
+LEFT JOIN {{ref ("stg_gz_raw_data__raw_gz_sales")}}
     using (products_id)
