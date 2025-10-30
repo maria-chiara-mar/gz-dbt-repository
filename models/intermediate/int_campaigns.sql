ed1b5ae14f45000fad7bcc335a8e3c65a73f5b4b
Maria Marrapodi <mchiaramarrapodi@gmail.com>
2025-10-30 16:05:16 +0000

    commit

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,17 @@
+SELECT*
+from stg_gz_raw_data__raw_gz_adwords
+
+UNION ALL
+
+SELECT*
+from stg_gz_raw_data__raw_gz_bing
+
+UNION ALL
+
+SELECT*
+from stg_gz_raw_data__raw_gz_criteo
+
+UNION ALL
+
+SELECT*
+from stg_gz_raw_data__raw_gz_facebook
